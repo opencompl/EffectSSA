e7899c1b71f2c7ba9595123e68b0bce62623970d
Alex Keizer <alex@keizer.dev>
2026-01-11 12:42:04 +0000

    write TODOs

--- EffectSSA/Trace/Defs.lean
+++ EffectSSA/Trace/Defs.lean
@@ -1,5 +1,7 @@
 import EffectSSA.Assumptions.MemorySignature
 
+import Mathlib.Data.Set.Defs
+
 /-!
 
 # Events & Traces
@@ -68,5 +70,38 @@ def append : Trace τ → Trace τ → Trace τ
   | .seq xs, .seq ys => .seq (xs ++ ys)
   | _, _ => .ub
 instance : Append (Trace τ) where append := append
+
+/--
+A `TraceZipper` points to a specific event in a (non-UB) trace, while also
+storing a list of all events that happend before and after.
+
+NOTE: Although this type is called a zipper, the chronological ordering of
+events in `pre` and `post` is the same.
+
+FIXME: Should this be renamed to something other than zipper to avoid confusion?
+FIXME: The namespace is duplicated in Trace.TraceZipper, should I find another name
+        and/or define it outside the Trace namespace? The name is not super
+       relevant, as I'm unlikely to explicitly name it much
+       outside of this file.
+-/
+structure TraceZipper τ [MemorySignature τ] where
+  pre : Trace τ
+  event : Event τ
+  post : Trace τ
+
+
+def events (ev : Trace τ) : Set (TraceZipper τ) :=
+  /-
+  TODO: write the body of `Trace.events`, so that I can write `⟨pre, e, post⟩ ∈ es.events`
+  -/
+  { }
+
+theorem eq_of_mem_events {es : Trace τ} (h : z ∈ es.events) :
+    es = z.pre ++ (z.event :> z.post) := by
+  /-
+  TODO: prove this characterization of `Trace.events` inhabitants
+  -/
+  sorry
+  -- FIXME: should this theorem be moved to a Lemmas file?
 
 end Trace
---
+++ TODO.md
@@ -0,0 +1,11 @@
+
+# Todo
+
+- [ ] Formalize lawfulness requirements (Assumptions/LawfulMemoryModel.lean)
+  - [ ] Write `Trace.events` (Trace/Defs.lean)
+
+- [ ] Define semantics
+  - [ ] load/store operations
+  - [ ] merge/split operations
+
+- [ ] Define syntax
