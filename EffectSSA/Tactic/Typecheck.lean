de4bd0bd35ed01a11805c0b12ab7ff9355ce9b88
Alex Keizer <alex@keizer.dev>
2026-02-21 11:05:21 +0000

    chore: add todo-note about typecheck and cbv

--- EffectSSA/Tactic/Typecheck.lean
+++ EffectSSA/Tactic/Typecheck.lean
@@ -6,8 +6,16 @@ import EffectSSA.Types
 
 This files defines a `typecheck` tactic, which automatically infers well-typedness
 of programs.
-
 -/
+
+/-
+TODO: I should port `typecheck` to use the new `cbv` tactic that exists in recent
+nigtlies, once I've actually updated to a recent-enough nightly.
+
+I should be able to reuse the decidability instance for something
+to actually reduce.
+-/
+
 namespace EffectSSA.Tactic
 
 macro "typecheck" : tactic => `(tactic|(
