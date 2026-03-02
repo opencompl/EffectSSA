c3b5aec52e806259da2d0e79d5c65f4d1d3f098d
Alex Keizer <alex@keizer.dev>
2026-03-02 15:39:24 +0000

    fix: prove eraseVars_nil and empty_eraseVars

--- EffectSSA/Types/Context/ListErase.lean
+++ EffectSSA/Types/Context/ListErase.lean
@@ -27,8 +27,20 @@ def eraseAllIdxP (p : Nat → Bool) (xs : List α) (n := 0) : List α :=
 
 @[simp, grind =] theorem eraseAllIdxP_nil : eraseAllIdxP p ([] : List α) = [] := by rfl
 
-@[simp, grind =] theorem eraseAllIdxP_cons {x : α} {xs : List α} :
+@[grind =] theorem eraseAllIdxP_cons {x : α} {xs : List α} :
     eraseAllIdxP p (x :: xs) n =
       let xs' := xs.eraseAllIdxP p (n + 1)
       if p n then xs' else x :: xs' := by
   simp [eraseAllIdxP]; grind
+
+/-- If `p` returns false on all inputs, `eraseAllIdxP p` has no effect. -/
+@[simp, grind =] theorem eraseAllIdxP_of_false {xs : List α}
+    (h : ∀ i < xs.length, p (i + n) = false) :
+    xs.eraseAllIdxP p n = xs := by
+  simp [eraseAllIdxP]
+  induction xs generalizing n
+  case nil => rfl
+  case cons _x xs ih =>
+    have : p n = false := by grind [h 0]
+    have h' (i : Nat) : i < xs.length → p (i + (n + 1)) = false := by grind [h (i + 1)]
+    simp; grind
