45fd9562dd3a00a53ed3587000f8427f2c05613b
Alex Keizer <alex@keizer.dev>
2026-03-16 16:53:33 +0000

    retain documentation about what compatibility of events means

--- EffectSSA/Assumptions/MemoryModel.lean
+++ EffectSSA/Assumptions/MemoryModel.lean
@@ -13,6 +13,16 @@ namespace EffectSSA
 ## MemoryModel class
 -/
 
+/--
+The `MemoryModel` of `τ` gives (1) a semantics to the `read` operation, (2) a
+notion of compatibility of (memory) events, and (3) a notion of which traces of
+events are legal.
+
+Intuitively, two events ought to be compatible if they may be freely reordered
+without changing the result of the events themselves (e.g., when either is a
+`load`) or any subsequent events. In other words, the events ought to be
+compatible iff their effects commute.
+-/
 class MemoryModel τ extends MemorySignature τ, Compat (Event τ) where
   /--
   `read ty p ev` should return the value (of type `ty`) stored at location `p`,
