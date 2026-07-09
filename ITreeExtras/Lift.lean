9f2320d661445a80d46f61043949175d499ef16d
Alex Keizer <alex@keizer.dev>
2026-07-09 17:21:38 +0100

    chore: hasEffect / mayReturn_lift lemma statements

--- ITreeExtras/Lift.lean
+++ ITreeExtras/Lift.lean
@@ -3,6 +3,8 @@ module
 public import ITreeExtras.Definition
 public import ITreeExtras.InterpM
 public import ITreeExtras.Basic
+public import ITreeExtras.Interp
+public import ITreeExtras.HasEffect
 
 /-!
 # Lifting ITrees along subeffects
@@ -28,7 +30,7 @@ namespace ITree
 Translate an ITree along a subeffect inclusion `[ε -< δ]`.
 -/
 def lift [ε -< δ] : ITree ε α → ITree δ α :=
-  interpM fun i =>
+  interp' fun i =>
     let ⟨j, k⟩ := Subeffect.map i
     .vis j (.ret ∘ k)
 
@@ -36,6 +38,23 @@ def lift [ε -< δ] : ITree ε α → ITree δ α :=
 class's first argument is an `outParam`, so we define `MonadLiftT` directly. -/
 instance [ε -< δ] : MonadLiftT (ITree ε) (ITree δ) where
   monadLift := lift
+
+section Lemmas
+
+/-! #### `liftM` -/
+
+@[simp, grind =]
+theorem hasEffect_liftM [ε₁ -< ε₂] (t : ITree ε₁ α) :
+    (liftM (n:=ITree ε₂) t).HasEffect e ↔
+      ∃ e', t.HasEffect e' ∧ (Subeffect.map e').fst = e := by
+  sorry
+
+@[simp, grind =]
+theorem mayReturn_liftM [ε₁ -< ε₂] (t : ITree ε₁ α) :
+    (liftM (n:=ITree ε₂) t).MayReturn x ↔ t.MayReturn x := by
+  sorry
+
+end Lemmas
 
 end ITree
 end ITree
