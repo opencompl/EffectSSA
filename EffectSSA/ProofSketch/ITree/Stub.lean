a0d2dc8df037d578b49c9adeba1dd079c437cde2
Alex Keizer <alex@keizer.dev>
2026-07-10 09:59:09 +0100

    post-rebase fixup: Stub

--- EffectSSA/ProofSketch/ITree/Stub.lean
+++ EffectSSA/ProofSketch/ITree/Stub.lean
@@ -8,13 +8,15 @@ public import EffectSSA.ProofSketch.ITree.InterpM
 -/
 @[expose] public section
 namespace ITree.ITree
-
-
+variable {ε} {κ : ε → Type _} [Effect ε κ] {α : Type _}
 
 def forever : ITree ε α :=
   .tau <| forever
 partial_fixpoint
 
 @[simp]
-axiom lift_forever [ε -< δ] : @lift ε δ α _ .forever = .forever
+axiom lift_forever
+    {ε δ} {κ₁ : ε → Type _} {κ₂ : δ → Type _}
+    [Effect ε κ₁] [Effect δ κ₂] [ε -< δ] {α : Type _} :
+    (liftM (n:=ITree δ) (forever (ε:=ε) (α:=α))) = forever
 attribute [grind .] lift_forever
