ab9f52b80b8ce4e85f286ccd9e15e43cc4e91d97
Alex Keizer <alex@keizer.dev>
2026-07-10 11:37:43 +0100

    feat: add nix devshell, instruct claude to use nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "EffectSSA dev shell with elan/lake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.elan ];
+      };
+    };
+}
