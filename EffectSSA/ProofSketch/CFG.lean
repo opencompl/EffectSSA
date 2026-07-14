42db6541aa5776d4db7b92a2fca643472c2f07ba
Alex Keizer <alex@keizer.dev>
2026-07-14 18:10:41 +0100

    feat: Hole.fromId?_id lemma

--- EffectSSA/ProofSketch/CFG.lean
+++ EffectSSA/ProofSketch/CFG.lean
@@ -78,13 +78,27 @@ where
     let some b := C.blocks[bId]? | raiseError s!"Missing Block: {bId}"
     b.denote bId args
 
-def Hole.fromId? {n} [ErrUB -< ε] (h : HoleId) : ITree ε (Hole n) :=
+/-! ### Hole API-/
+namespace Hole
+
+def fromId? {n} [ErrUB -< ε] (h : HoleId) : ITree ε (Hole n) :=
   if hr : h.toNat < n then
     return ⟨h.toNat, hr⟩
   else
     raiseError s!"Unknown hole: {h}"
 
-abbrev Hole.elim0 : Hole 0 → α := Fin.elim0
+abbrev elim0 : Hole 0 → α := Fin.elim0
+
+section Lemmas
+variable [ErrUB -< ε]
+
+@[simp, grind =]
+theorem fromId?_id (h : Hole n) :
+    (Hole.fromId? h.id : ITree ε (Hole n)) = ITree.ret h := by
+  simp [Hole.fromId?, Hole.id]
+
+end Lemmas
+end Hole
 
 noncomputable
 def ContextCFG.interp (C : ContextCFG n) (f : Hole n → ITree (ErrUB ⊕ InstEff) Unit) :
