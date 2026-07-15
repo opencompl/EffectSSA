6df0305d446febe9bd339e90fa82bf41c64df083
Alex Keizer <alex@keizer.dev>
2026-07-15 14:08:58 +0100

    refactor: define `ITree.map` as a primitive for `lift` which takes explicit maps (instead of maps from a Subeffect instance)

--- ITreeExtras/Lift.lean
+++ ITreeExtras/Lift.lean
@@ -11,6 +11,11 @@ public import ITreeExtras.HasEffect
 
 Given an inclusion of effects `[ε -< δ]`, we can lift a computation from
 `ITree ε α` into `ITree δ α` by translating each visible effect.
+
+The primitive operation is `ITree.map`, which translates an ITree along an
+explicit pair of maps on effects and continuations. `ITree.lift` is then
+defined as the specialization of `map` at the maps supplied by a
+`Subeffect` instance.
 -/
 
 @[expose] public section
@@ -25,17 +30,23 @@ instance instCoeTOfSubeffect [ε -< δ] {e} : CoeT ε e δ where
 end Effect
 
 namespace ITree
-open Subeffect (map mapEff mapCont)
+
+/--
+Translate an ITree along explicit maps on effects and continuations.
+-/
+def map (fEff : ε → δ) (fCont : (i : ε) → κδ (fEff i) → κε i)
+    (t : ITree ε α) : ITree δ α :=
+  match t.unfold with
+  | .ret x => .ret x
+  | .tau u => .tau (u.map fEff fCont)
+  | .vis i k => .vis (fEff i) (fun o => (k (fCont i o)).map fEff fCont)
+  partial_fixpoint
 
 /--
 Translate an ITree along a subeffect inclusion `[ε -< δ]`.
 -/
-def lift [ε -< δ] (t : ITree ε α) : ITree δ α :=
-  match t.unfold with
-  | .ret x => .ret x
-  | .tau u => .tau u.lift
-  | .vis i k => .vis (mapEff i) (fun o => k (mapCont i o) |>.lift)
-  partial_fixpoint
+abbrev lift [ε -< δ] (t : ITree ε α) : ITree δ α :=
+  t.map Subeffect.mapEff Subeffect.mapCont
 
 /-- NOTE: the following instance cannot be defined on `MonadLift`, given that
 class's first argument is an `outParam`, so we define `MonadLiftT` directly. -/
@@ -46,48 +57,47 @@ instance [ε -< δ] : MonadLiftT (ITree ε) (ITree δ) where
 ## Lemmas
 -/
 section Lemmas
-variable [ε -< δ]
 
 /-! ### Basic -/
 section Basic
+variable {fEff : ε → δ} {fCont : (i : ε) → κδ (fEff i) → κε i}
 
-@[simp, grind =] theorem lift_ret (r : α) :
-    lift (ε := ε) (δ := δ) (ret r) = ret r := by
-  simp [lift]
+@[simp, grind =] theorem map_ret (r : α) :
+    map fEff fCont (ret r) = ret r := by
+  simp [map]
 
-@[simp, grind =] theorem lift_tau (t : ITree ε α) :
-    lift (δ := δ) (tau t) = tau (lift t) := by
-  conv => {lhs; rw [lift]}
+@[simp, grind =] theorem map_tau (t : ITree ε α) :
+    map fEff fCont (tau t) = tau (map fEff fCont t) := by
+  conv => {lhs; rw [map]}
   simp
 
-@[simp, grind =] theorem lift_vis (i : ε) (k : κε i → ITree ε α) :
-    lift (δ := δ) (vis i k)
-    = vis (mapEff i) (fun o => k (mapCont i o) |>.lift) := by
-  conv => {lhs; rw [lift]}
+@[simp, grind =] theorem map_vis (i : ε) (k : κε i → ITree ε α) :
+    map fEff fCont (vis i k)
+    = vis (fEff i) (fun o => (k (fCont i o)).map fEff fCont) := by
+  conv => {lhs; rw [map]}
   simp
 
-theorem liftM_eq_lift (t : ITree ε α) :
+theorem liftM_eq_lift [ε -< δ] (t : ITree ε α) :
     liftM (n:=ITree δ) t = lift (δ := δ) t := rfl
 
-@[simp, grind =] theorem lift_eq_ret_iff (t : ITree ε α) (r : α) :
-    t.lift (δ:=δ) = ret r ↔ t = ret r := by
-  rw [lift]; grind
+@[simp, grind =] theorem map_eq_ret_iff (t : ITree ε α) (r : α) :
+    map fEff fCont t = ret r ↔ t = ret r := by
+  rw [map]; grind
 
-@[simp] theorem lift_eq_tau_iff (t : ITree ε α) :
-    t.lift (δ:=δ) = tau t' ↔ ∃ u, t = tau u ∧ t' = u.lift := by
-  rw [lift]; grind
+@[simp] theorem map_eq_tau_iff (t : ITree ε α) :
+    map fEff fCont t = tau t' ↔ ∃ u, t = tau u ∧ t' = map fEff fCont u := by
+  rw [map]; grind
 
-open Subeffect (map) in
-@[simp] theorem lift_eq_vis_iff (t : ITree ε α) :
-    t.lift (δ:=δ) (κδ:=κδ) = vis i k ↔ ∃ j l,
+@[simp] theorem map_eq_vis_iff (t : ITree ε α) :
+    map fEff fCont t = vis i k ↔ ∃ j l,
       t = vis j l
-      ∧ i = (map j).1
-      ∧ k ≍ (fun x => (l ((map (ε₂:=δ) j).2 x)).lift (δ:=δ)) := by
+      ∧ i = fEff j
+      ∧ k ≍ (fun x => (l (fCont j x)).map fEff fCont) := by
   cases t
   case ret => simp
   case tau => simp
   case vis j l =>
-    simp only [lift_vis, vis_inj]
+    simp only [map_vis, vis_inj]
     constructor
     · grind
     · rintro ⟨j, l, ⟨rfl, rfl⟩, rfl, rfl⟩
@@ -98,31 +108,30 @@ end Basic
 /-! ### MayReturn -/
 section MayReturn
 
-open Subeffect (map) in
-@[simp, grind =]
-theorem mayReturn_liftM (t : ITree ε α) :
-    (liftM (n:=ITree δ) t).MayReturn x ↔ t.MayReturn x := by
-  rw [liftM_eq_lift]
+theorem mayReturn_map {fEff : ε → δ} {fCont : (i : ε) → κδ (fEff i) → κε i}
+    (surj : ∀ i, Function.Surjective (fCont i)) (t : ITree ε α) :
+    (map fEff fCont t).MayReturn x ↔ t.MayReturn x := by
   constructor
-  · generalize ht : t.lift (δ:=δ) = t'
+  · generalize ht : map fEff fCont t = t'
     intro h
     induction h generalizing t
     case ret r _ =>
-      replace ht : t.lift (δ:=δ) = .ret r := by grind
+      replace ht : map fEff fCont t = .ret r := by grind
       obtain rfl : t = .ret r := by simp_all
       grind
     case tau r t' _ hr ih =>
-      replace ht : t.lift (δ:=δ) = .tau t' := by grind
-      obtain ⟨u, rfl, rfl⟩ : ∃ u, t = tau u ∧ t' = u.lift := by simpa using ht
+      replace ht : map fEff fCont t = .tau t' := by grind
+      obtain ⟨u, rfl, rfl⟩ : ∃ u, t = tau u ∧ t' = map fEff fCont u := by
+        simpa using ht
       grind
     case vis r i k o _ hr ih =>
-      replace ht : t.lift (δ:=δ) = .vis i k := by grind
+      replace ht : map fEff fCont t = .vis i k := by grind
       obtain ⟨j, l, ⟨rfl, rfl⟩, rfl, rfl⟩ : ∃ j l,
           t = vis j l
-          ∧ i = (map j).fst
-          ∧ k ≍ fun x => (l ((map j).snd x)).lift := by
+          ∧ i = fEff j
+          ∧ k ≍ fun x => (l (fCont j x)).map fEff fCont := by
         simpa using ht
-      specialize ih (l ((map j).snd o))
+      specialize ih (l (fCont j o))
       grind
   · intro h
     induction h
@@ -131,25 +140,29 @@ theorem mayReturn_liftM (t : ITree ε α) :
     case vis t r i k o ht hk hkl =>
       replace ht : t = vis i k := by grind
       subst ht
-      let mi := (map (ε₂:=δ) i).2
-      obtain ⟨o, rfl⟩ : ∃ o', mi o' = o := by
-        suffices mi.Surjective by apply this
-        grind
+      obtain ⟨o, rfl⟩ : ∃ o', fCont i o' = o := surj i o
       grind
+
+@[simp, grind =]
+theorem mayReturn_liftM [ε -< δ] (t : ITree ε α) :
+    (liftM (n:=ITree δ) t).MayReturn x ↔ t.MayReturn x := by
+  rw [liftM_eq_lift]
+  exact mayReturn_map (by grind) t
 
 end MayReturn
 
 /-! ### HasEffect -/
 section HasEffect
 
-@[simp, grind =]
-theorem hasEffect_liftM (t : ITree ε α) {e : δ} :
-    (liftM (n:=ITree δ) t).HasEffect e ↔
-      ∃ e', t.HasEffect e' ∧ mapEff e' = e := by
-  rw [liftM_eq_lift]
+theorem hasEffect_map {fEff : ε → δ} {fCont : (i : ε) → κδ (fEff i) → κε i}
+    (surj : ∀ i, Function.Surjective (fCont i))
+    (t : ITree ε α) {e : δ} :
+    (map fEff fCont t).HasEffect e ↔
+      ∃ e', t.HasEffect e' ∧ fEff e' = e := by
   constructor
-  · suffices h : ∀ (t' : ITree δ α) (t : ITree ε α), t' = t.lift → t'.HasEffect e →
-        ∃ e', t.HasEffect e' ∧ mapEff e' = e by
+  · suffices h : ∀ (t' : ITree δ α) (t : ITree ε α),
+        t' = map fEff fCont t → t'.HasEffect e →
+        ∃ e', t.HasEffect e' ∧ fEff e' = e by
       grind
     intro t' t ht h_eff
     induction h_eff generalizing t with
@@ -160,19 +173,22 @@ theorem hasEffect_liftM (t : ITree ε α) {e : δ} :
       | ret r => simp_all
       | tau t' => simp_all
       | vis i₀ k₀ =>
-        obtain rfl : mapEff i₀ = i' := by grind
-        specialize ih (k₀ (mapCont i₀ o))
+        obtain rfl : fEff i₀ = i' := by grind
+        specialize ih (k₀ (fCont i₀ o))
         grind
   · rintro ⟨e', he', rfl⟩
     induction he' with
     | vis_self => grind
     | tau => grind
     | @vis_cont t' j j' k o hu h ih =>
-      let mi := (map (ε₂:=δ) j').2
-      obtain ⟨o, rfl⟩ : ∃ o', mi o' = o := by
-        suffices mi.Surjective by apply this
-        grind
+      obtain ⟨o, rfl⟩ : ∃ o', fCont j' o' = o := surj j' o
       grind
+
+@[simp, grind =]
+theorem hasEffect_lift [ε -< δ] (t : ITree ε α) {e : δ} :
+    (lift (δ:=δ) t).HasEffect e ↔
+      ∃ e', t.HasEffect e' ∧ Subeffect.mapEff e' = e :=
+  hasEffect_map (by grind) t
 
 end HasEffect
 
@@ -180,22 +196,21 @@ end HasEffect
 section Interp
 variable {η κη} [Effect.{u} η κη]
 
-open Subeffect (mapEff mapCont)
-
-/-- Interpretation composes with `lift` -/
-theorem interp_lift (f : δ ⤳ ITree η) (t : ITree ε α) :
-    interp f (lift (δ := δ) t)
+/-- Interpretation composes with `map` -/
+theorem interp_map (fEff : ε → δ) (fCont : (i : ε) → κδ (fEff i) → κε i)
+    (f : δ ⤳ ITree η) (t : ITree ε α) :
+    interp f (map fEff fCont t)
       = interp (fun i : ε => do
-          let x ← (f (mapEff i))
-          return mapCont _ x
+          let x ← f (fEff i)
+          return fCont i x
           ) t := by
   apply eq_of_bisim
   apply Bisim.coinduct (fun (x y : ITree η α) =>
     ∃ (α₀ : Type u) (u : ITree η α₀) (k : α₀ → ITree ε α),
-        x = u >>= (fun a => interp f (lift (δ := δ) (k a)))
+        x = u >>= (fun a => interp f (map fEff fCont (k a)))
       ∧ y = u >>= (fun a => interp (fun i : ε => do
-          let x ← (f (mapEff i))
-          return mapCont i x) (k a)))
+          let x ← f (fEff i)
+          return fCont i x) (k a)))
   · rintro x y ⟨α₀, u, k, rfl, rfl⟩
     cases u with
     | tau u' =>
@@ -211,32 +226,41 @@ theorem interp_lift (f : δ ⤳ ITree η) (t : ITree ε α) :
       cases k a with
       | ret r => simp
       | tau t' =>
-        simp only [lift_tau, interp_tau]
+        simp only [map_tau, interp_tau]
         right; left
         refine ⟨_, _, ⟨_, ret ⟨⟩, fun _ : PUnit => t', ?_⟩, rfl, rfl⟩
         simp
       | vis i k'' =>
-        simp only [lift_vis, interp_vis, bind_assoc, pure_bind]
-        cases f (mapEff i) with
+        simp only [map_vis, interp_vis, bind_assoc, pure_bind]
+        cases f (fEff i) with
         | ret o =>
           simp only [pure_bind]
           right; left
-          refine ⟨_, _, ⟨_, ret ⟨⟩, fun _ : PUnit => k'' (mapCont i o), ?_⟩, rfl, rfl⟩
+          refine ⟨_, _, ⟨_, ret ⟨⟩, fun _ : PUnit => k'' (fCont i o), ?_⟩, rfl, rfl⟩
           simp
         | tau t' =>
           simp only [tau_bind]
           right; left
-          refine ⟨_, _, ⟨_, t', fun x => tau (k'' (mapCont i x)), ?_⟩, rfl, rfl⟩
+          refine ⟨_, _, ⟨_, t', fun x => tau (k'' (fCont i x)), ?_⟩, rfl, rfl⟩
           simp
         | vis j k''' =>
           simp only [vis_bind]
           right; right
           refine ⟨j, _, _,
-                  fun o' => ⟨_, k''' o', fun x => tau (k'' (mapCont i x)), ?_⟩,
+                  fun o' => ⟨_, k''' o', fun x => tau (k'' (fCont i x)), ?_⟩,
                   rfl, rfl⟩
           simp
   · refine ⟨PUnit, ret ⟨⟩, fun _ => t, ?_⟩
     simp
+
+/-- Interpretation composes with `lift` -/
+theorem interp_lift [ε -< δ] (f : δ ⤳ ITree η) (t : ITree ε α) :
+    interp f (lift (δ := δ) t)
+      = interp (fun i : ε => do
+          let x ← f (Subeffect.mapEff i)
+          return Subeffect.mapCont i x
+          ) t :=
+  interp_map _ _ f t
 
 end Interp
 
