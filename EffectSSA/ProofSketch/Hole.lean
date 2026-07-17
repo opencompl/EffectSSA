719f21d259790b4edea6fd0f678aef24440903b2
Alex Keizer <alex@keizer.dev>
2026-07-17 16:15:24 +0100

    feat: Hole.elim0 normalization simproc (#107)

--- EffectSSA/ProofSketch/Hole.lean
+++ EffectSSA/ProofSketch/Hole.lean
@@ -1,5 +1,6 @@
 module
 
+public import Lean.Elab.Tactic.Simp
 
 /-!
 # Holes
@@ -44,7 +45,7 @@ returning the raw underlying `HoleId`.
 def id (h : Hole n) : HoleId where
   toNat := h.val
 
-abbrev elim0 : Hole 0 → α := Fin.elim0
+def elim0 : Hole 0 → α := Fin.elim0
 
 def fromId? {n} (h : HoleId) : Option (Hole n) :=
   if hr : h.toNat < n then
@@ -52,4 +53,55 @@ def fromId? {n} (h : HoleId) : Option (Hole n) :=
   else
     none
 
+section Lemmas
+
+@[simp, grind =]
+theorem fromId?_id (h : Hole n) :
+    (Hole.fromId? h.id) = some h := by
+  simp [Hole.fromId?, Hole.id]
+
+theorem eq_elim0 {α : Sort u} (f : Hole 0 → α) : f = Hole.elim0 :=
+  funext fun h => h.elim0
+
+end Lemmas
 end Hole
+
+/-!
+### `Hole.elim0` normalization simproc
+--------------------------------------------------------------------------------
+-/
+section Meta
+
+open Lean Meta Simp Elab
+
+/--
+Normalize any lambda `fun (_ : Hole 0) => _` to `Hole.elim0`.
+
+We restrict the pattern to *lambdas* (rather than arbitrary terms of type
+`Hole 0 → α`) to avoid the simproc firing on every term; as it stands, this
+simproc might fire on *every* lambda, also those that have a different
+domain type. [1]
+
+For the same reason, we make this simproc `scoped`.
+
+[1] AFAICT, simprocs are *not* indexed on the type of the expression, only on
+    the actual expression, and it *seems* that for lambdas, this does not
+    include the type of the argument.
+-/
+scoped simproc ↓ EffectSSA.ProofSketch.hole0_elim0_simproc
+    (fun (_ : EffectSSA.ProofSketch.Hole 0) => _) := fun e => do
+  let Expr.lam _ dom _ _ := e | return Step.continue
+  let Hole0 := mkApp (mkConst ``EffectSSA.ProofSketch.Hole) (mkNatLit 0)
+  unless ← withReducible <| isDefEq dom Hole0 do
+      return Step.continue
+  let Expr.forallE _ _ codomain _ ← inferType e | return Step.continue
+  if codomain.hasLooseBVars then return Step.continue
+  let u ← getLevel codomain
+  let elim0 := mkApp (mkConst ``EffectSSA.ProofSketch.Hole.elim0 [u]) codomain
+  let proof := mkApp2 (mkConst ``EffectSSA.ProofSketch.Hole.eq_elim0 [u]) codomain e
+  return Step.done { expr := elim0, proof? := proof }
+
+/-! Simple smoke test -/
+example : (fun _ : Hole 0 => 1) = (fun _ => 2) := by simp
+
+end Meta
